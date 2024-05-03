6e836138599352d88d397f035781b1be9dc05fae
AravindCodesAllDay <aravindsiva2003.in@gmail.com>
2024-05-03 09:53:20 +0530

    day-3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../pages/home/home.component';
 import { AboutComponent } from '../pages/about/about.component';
-import { ApplicationComponent } from './application/application.component';
-import { ParkingEcoComponent } from './parking-eco/parking-eco.component';
+import { ApplicationComponent } from '../pages/application/application.component';
+import { ParkingEcoComponent } from '../pages/parking-eco/parking-eco.component';
 
 export const routes: Routes = [
   {
